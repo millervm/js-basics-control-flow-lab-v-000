1775e77648144a98acb794a04353ae36a628236a
Veronica Miller <vmcmiller@gmail.com>
2018-09-29 21:14:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let greeting;
+  if (feet <= 400) {
+    greeting = "This one is on me!";
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  }
+  else if (feet > 2500) {
+    greeting = "No can do.";
+  }
+  return greeting;
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
+  return response;
+}
